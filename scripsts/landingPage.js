d391115ccb941fdb4e467339ccbcdb7f06aa2b3a
justabrocolli <157285880+justabrocolli@users.noreply.github.com>
2024-01-22 11:26:12 +0200

    Add files via upload

--- scripsts/landingPage.js
+++ scripsts/landingPage.js
@@ -1,6 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("open-popup-button").addEventListener("click", sign_up_popup);
+  document.getElementById("open-login-popup-button").addEventListener("click", login_popup);
 });
 
 function sign_up_popup() {
@@ -14,6 +15,21 @@ function sign_up_close() {
   let popup = document.getElementById("show-form");
   if (popup) {
     popup.classList.remove("form-opened");
+  }
+}
+
+//log in form
+function login_popup() {
+  let loginPopup = document.getElementById("login_form");
+  if (loginPopup) {
+    loginPopup.classList.add("login_opened");
+  }
+}
+
+function login_popup_close() {
+  let loginPopup = document.getElementById("login_form");
+  if (loginPopup) {
+    loginPopup.classList.remove("login_opened");
   }
 }
 
